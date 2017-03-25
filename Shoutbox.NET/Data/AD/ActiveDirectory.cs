c5c43ad9ecff6f5b3b9f5a1c992679a810426d95
Bubuchenko <kirichenkobubu@gmail.com>
2017-03-25 14:52:50 +0100

    Added ActiveDirectory service objects and function to query AD users

---
+++ Shoutbox.NET/Data/AD/ActiveDirectory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.DirectoryServices;
+
+namespace Shoutbox.NET.Data.AD
+{
+
+    public class ActiveDirectory : IActiveDirectory
+    {
+        public UserPrincipal GetUser(string username)
+        {
+            // Enter AD settings  
+            PrincipalContext AD = new PrincipalContext(ContextType.Domain, ConfigurationManager.AppSettings["ActiveDirectoryDomain"]);
+
+            // Create search user and add criteria  
+            UserPrincipal u = new UserPrincipal(AD);
+            u.SamAccountName = username;
+
+            // Search for user  
+            using (PrincipalSearcher search = new PrincipalSearcher(u))
+            {
+                (search.GetUnderlyingSearcher() as DirectorySearcher).PropertiesToLoad.Clear();
+                (search.GetUnderlyingSearcher() as DirectorySearcher).PropertiesToLoad.Add("samaccountname");
+                (search.GetUnderlyingSearcher() as DirectorySearcher).PropertiesToLoad.Add("displayname");
+                (search.GetUnderlyingSearcher() as DirectorySearcher).PropertiesToLoad.Add("department");
+                return (UserPrincipal)search.FindOne();
+            }
+        }
+    }
+}
---
+++ Shoutbox.NET/Data/AD/IActiveDirectory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices.AccountManagement;
+
+namespace Shoutbox.NET.Data.AD
+{
+    interface IActiveDirectory
+    {
+        UserPrincipal GetUser(string username);
+
+    }
+}
